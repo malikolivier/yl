13110fc0ab7fe9b47a057c69ee303e2dfd046984
Malik Olivier Boussejra <malik@boussejra.com>
2017-12-08 21:56:25 +0900

    yl: Add interactive mode

--- lisp-js/yl.js
+++ lisp-js/yl.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
+const readline = require('readline');
 
 const Parser = require('./parser');
 const Interpreter = require('./interpreter');
 
 var inputFile = process.argv[2];
 if (!inputFile) {
-    console.error('No input!');
-    process.exit(1);
-}
-var code = fs.readFileSync(inputFile, 'utf8');
-var ast = Parser(code);
-var ret = Interpreter(ast);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    process.stdout.write('> ');
+    rl.on('line', function (cmd) {
+        var ast = Parser(cmd);
+        var ret = Interpreter(ast);
+        console.log(ret);
+        process.stdout.write('> ');
+    });
+} else {
+    var code = fs.readFileSync(inputFile, 'utf8');
+    var ast = Parser(code);
+    var ret = Interpreter(ast);
 
-process.exit(ret);
+    process.exit(ret);
+}
