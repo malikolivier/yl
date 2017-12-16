827810797d874641d72b0f1619d344bbb5b88817
Malik Olivier Boussejra <malik@boussejra.com>
2017-12-16 21:13:55 +0900

    test.sh: Add rust in unit tests

--- test.sh
+++ test.sh
@@ -11,11 +11,15 @@ elif [ "$1" = "python" ]; then
 elif [ "$1" = "c" ]; then
 	echo -n "C	"
 	INTERPRETER="c/yl"
+elif [ "$1" = "rust" ]; then
+	echo -n "Rust	"
+	INTERPRETER="rust/target/release/yl"
 else
 	# Run all languages!
 	$0 node
 	$0 python
 	$0 c
+	$0 rust
 	exit
 fi
 
