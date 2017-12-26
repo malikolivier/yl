906e804828fd3fb8454a674f2dc06afa305edb03
Malik Olivier Boussejra <malik@boussejra.com>
2017-12-27 02:46:10 +0900

    interpreter.rb: Define most stuff except def

--- ruby/interpreter.rb
+++ ruby/interpreter.rb
@@ -1,10 +1,87 @@
 YL_FALSE = nil
 YL_TRUE = 1
 
+GLOBAL_VARS = {
+  'print' => lambda { |args, _scope|
+    if args[0] == YL_FALSE
+      puts '()'
+    elsif args[0].is_a? Float
+      puts args[0].to_i
+    else
+      puts args[0]
+    end
+  },
+  '!' => lambda { |args, _scope|
+    if args[0] == YL_FALSE
+      YL_TRUE
+    else
+      YL_FALSE
+    end
+  },
+  '=' => lambda { |args, _scope|
+    if args[0] == args[1]
+      YL_TRUE
+    else
+      YL_FALSE
+    end
+  },
+  '<' => lambda { |args, _scope|
+    if args[0] < args[1]
+      YL_TRUE
+    else
+      YL_FALSE
+    end
+  },
+  '<=' => lambda { |args, _scope|
+    if args[0] <= args[1]
+      YL_TRUE
+    else
+      YL_FALSE
+    end
+  },
+  '>' => lambda { |args, _scope|
+    if args[0] > args[1]
+      YL_TRUE
+    else
+      YL_FALSE
+    end
+  },
+  '>=' => lambda { |args, _scope|
+    if args[0] >= args[1]
+      YL_TRUE
+    else
+      YL_FALSE
+    end
+  },
+  '+' => lambda { |args, _scope|
+    args[0] + args[1]
+  },
+  '-' => lambda { |args, _scope|
+    args[0] - args[1]
+  },
+  '*' => lambda { |args, _scope|
+    args[0] * args[1]
+  },
+  '/' => lambda { |args, _scope|
+    args[0] / args[1]
+  },
+  '%' => lambda { |args, _scope|
+    args[0] % args[1]
+  },
+  'let' => lambda { |args, scope|
+    scope.set(args[0], args[1])
+    args[1]
+  }
+}.freeze
+
 # Scope for YL
 class Scope
   def initialize(parent = nil)
-    @vars = {}
+    @vars = if parent.nil?
+              GLOBAL_VARS.dup
+            else
+              {}
+            end
     @parent = parent
   end
 
@@ -36,6 +113,15 @@ class AST
       evaluate_singleton(scope)
     elsif evaluate_function && function_to_execute?(scope)
       # Evaluate function
+      case @ast[0]
+      when 'def'
+        scope.get('def').call(@ast.drop(1), scope)
+      else
+        args = @ast.drop(1).map do |subast|
+          AST.new(subast).evaluate(scope)
+        end
+        scope.get(@ast[0]).call(args, scope)
+      end
     else
       ret = YL_FALSE
       @ast.each do |subast|
@@ -51,7 +137,7 @@ class AST
     ret = scope.get(@ast)
     if ret
       ret
-    elsif /^[0-9]+\.?[0-9]*$/ =~ ret
+    elsif /^[0-9]+\.?[0-9]*$/ =~ @ast
       @ast.to_f
     else
       @ast
