11af92654d707d1a079550e657f5a4fca2d3c0ec
Malik Olivier Boussejra <malik@boussejra.com>
2018-01-11 17:38:49 +0900

    yl.hs: Implement 'argv'

--- haskell/yl.hs
+++ haskell/yl.hs
@@ -8,21 +8,59 @@ import Parser
 
 main = do
     args <- getArgs
-    program <- getProgram args
-    runProgram program
+    code <- getCode args
+    runProgram $ createProgram code args
 
+getCode :: [[Char]] -> IO [Char]
 -- Interactive mode
-getProgram [] = do
+getCode [] = do
     hSetBuffering stdout NoBuffering
     interativeMode globalScope
     error "Interactive mode is a loop!"
-getProgram ("-e":program:[]) = return program
-getProgram (file:[]) = do
+getCode ("-e":program:_) = return program
+getCode (file:_) = do
     handle <- openFile file ReadMode
     hGetContents handle
 
-runProgram :: [Char] -> IO ()
-runProgram program = let Context {var=_, io=io, scope=_} = evaluateGlobal $ parse program in
+createProgram :: [Char] -> [[Char]] -> Ast
+createProgram code ("-e":_:ylArgs) = addArgvProgram ylArgs $ parse code
+createProgram code (file:ylArgs) = addArgvProgram ylArgs $ parse code
+
+addArgvProgram :: [[Char]] -> Ast -> Ast
+addArgvProgram ylArgs (AstList list) = AstList $ (createArgvProgram ylArgs):list
+
+createArgvProgram :: [[Char]] -> Ast
+createArgvProgram args = AstList ([ AstNode "def"
+                                  , AstNode "argv"
+                                  , AstList [AstNode "n"]
+                                  ]
+                                  ++ createArgsDefinition args 0
+                                  ++ [createArgsBranches args 0])
+
+createArgsDefinition :: [[Char]] -> Int -> [Ast]
+createArgsDefinition [] _ = []
+createArgsDefinition (arg:next) counter =
+    let counterStr = show counter in
+        (AstList [ AstNode "let"
+                 , AstNode ("__arg" ++ counterStr ++ "__")
+                 , AstNode arg
+                 ]) : (createArgsDefinition next (counter + 1))
+
+createArgsBranches ::  [[Char]] -> Int -> Ast
+createArgsBranches [] _ = AstList []
+createArgsBranches (arg:next) counter =
+    let counterStr = show counter in
+        AstList [ AstNode "if"
+                , AstList [ AstNode "="
+                          , AstNode "n"
+                          , AstNode counterStr
+                          ]
+                , AstList [ AstNode ("__arg" ++ counterStr ++ "__") ]
+                , AstList [createArgsBranches next (counter + 1)]
+                ]
+
+runProgram :: Ast -> IO ()
+runProgram program = let Context {var=_, io=io, scope=_} = evaluateGlobal program in
     io
 
 interativeMode :: Scope -> IO ()
