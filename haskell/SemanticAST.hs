18d1d7390da87097c702afd569b7863c59157c47
Malik Olivier Boussejra <malik@boussejra.com>
2018-02-06 14:53:15 +0900

    SemanticAST.hs: Implement let

--- haskell/SemanticAST.hs
+++ haskell/SemanticAST.hs
@@ -88,7 +88,7 @@ semantic_parse_list ctx (h:next) False =
 semantic_parse_list ctx all@(h:next) True =
     case h of
         AstList list    -> semantic_parse_list ctx all False
-        AstNode "let"   -> undefined
+        AstNode "let"   -> semantic_parse_let ctx next
         AstNode "def"   -> undefined
         AstNode "if"    -> undefined
         AstNode "loop"  -> undefined
@@ -97,6 +97,19 @@ semantic_parse_list ctx all@(h:next) True =
             case var of
                 Nothing  -> semantic_parse_list ctx all False
                 Just id_ -> sementic_parse_call ctx id_ next
+
+semantic_parse_let :: SemanticParseContext -> [Ast] -> SemanticParseContext
+semantic_parse_let ctx (lhs:[]) = semantic_parse_let ctx [lhs, AstList []]
+semantic_parse_let ctx (lhs:rhs:_) =
+    case lhs of
+        AstList _          -> error("Expect an identifier after 'let'")
+        AstNode identifier ->
+            let ctx' = semantic_parse_temp ctx rhs True
+                scope' = scope_set (scope ctx') identifier
+                semAst' = LetNode { let_identifier=unwrap_maybe $ scope_get scope' identifier
+                                  , let_rhs=semAst ctx'}
+            in
+            SemanticParseContext { semAst = semAst', scope=scope' }
 
 sementic_parse_call :: SemanticParseContext -> IdentifierNode -> [Ast] -> SemanticParseContext
 sementic_parse_call ctx id_ args =
@@ -113,6 +126,9 @@ sementic_parse_call ctx id_ args =
             in
             (ctx'', semAst ctx' : nextArgs)
 
+unwrap_maybe :: Maybe t -> t
+unwrap_maybe (Just m) = m
+unwrap_maybe _ = error("Cannot unwrap this maybe. This IS a compiler bug.")
 
 scope_get :: Scope -> String -> Maybe IdentifierNode
 scope_get scope id_ =
@@ -127,6 +143,52 @@ scope_get scope id_ =
         find [] _ = Nothing
         find (h:next) id_ =
             if id_ == id_symbol h then Just h else find next id_
+
+ -- Must create IdentifierNode (they should be counted!)
+ -- Raise an error if identifier already exists in the same child scope!
+scope_set :: Scope -> String -> Scope
+scope_set scope str =
+    let vars = get_current_scope_vars scope
+    in
+    case find vars str of
+        Just idNode -> error("Variable '" ++ str ++ "' already defined!")
+        Nothing     -> set_value scope str
+    where
+        get_current_scope_vars :: Scope -> [IdentifierNode]
+        get_current_scope_vars scope =
+            case scope of
+                TopLevel vars                  -> vars
+                ChildScope { scope_vars=vars } -> vars
+        find :: [IdentifierNode] -> String -> Maybe IdentifierNode
+        find [] _ = Nothing
+        find (h:next) id_ =
+            if id_ == id_symbol h then Just h else find next id_
+
+        set_value :: Scope -> String -> Scope
+        set_value scope str =
+            let c = count_id scope str
+            in
+            case scope of
+                TopLevel vars -> TopLevel (add_id_node vars str c)
+                all@ChildScope { scope_vars=vars } ->
+                    all { scope_vars=add_id_node vars str c }
+            where
+                count_id :: Scope -> String -> Integer
+                count_id scope str =
+                    case scope of
+                        TopLevel vars ->
+                            count_id_in_list vars str
+                        ChildScope {scope_vars=vars, scope_parent=p} ->
+                            count_id_in_list vars str + count_id p str
+                    where
+                        count_id_in_list :: [IdentifierNode] -> String -> Integer
+                        count_id_in_list vars str =
+                            case find vars str of
+                                Nothing -> 0
+                                Just _  -> 1
+                add_id_node :: [IdentifierNode] -> String -> Integer -> [IdentifierNode]
+                add_id_node vars str c =
+                    IdentifierNode {id_symbol=str, id_count=c, captured_vars=[]} : vars
 
 parse_string :: String -> SemanticAST
 parse_string string =
